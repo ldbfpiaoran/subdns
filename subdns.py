3daa14c32e3771ea31fa2aa04b6a40acf7ffd807
bfpiaoran <207426719@qq.com>
2020-08-13 22:44:47 +0800

    add fuzz

--- subdns.py
+++ subdns.py
@@ -35,6 +35,8 @@ log.setLevel(logging.INFO)
 
 class Subscan:
     def __init__(self, paras={}):
+        self.is_fuzz = paras['fuzz'] if paras.get('fuzz') else False
+        self.fuzz_data = paras['fd'] if paras.get('fd') else ""
         self.deep = paras['deep'] if paras.get('deep') else 5
         self.test = paras['test'] if paras.get('test') else False
         self.check_analysis = True if paras.get('analysis_domain') else False  # 通过cname 判断泛解析 这个方法极度损耗性能相当于查询两遍dns
@@ -164,7 +166,7 @@ class Subscan:
                         f.write(subname+"\t"+str(ips)+"\n")
                 sub_deep = self.get_deep(subname)
                 sub_text = subname.replace("."+self.domain, "")
-                if sub_deep <= self.deep:   # 域名深度
+                if sub_deep <= self.deep and not self.is_fuzz:   # 域名深度
                     with open('dict/' + self.sec_dictname, 'r') as f:
                         for line in f:
                             self.queue.put_nowait(line.strip().lower()+"."+sub_text)
@@ -181,6 +183,8 @@ class Subscan:
         with open('dict/'+self.dictname, 'r') as f:
             for line in f:
                 domain = line.strip().lower()
+                if self.is_fuzz:   #  加个fuzz逻辑 such  mozi-console.alibaba.com   mozi-FUZZ.alibaba.com
+                    domain = self.fuzz_data.replace("FUZZ",domain)
                 if not self.check_bk_domain(domain):
                     self.queue.put_nowait(domain)
         brute_tasks = [self.loop.create_task(self.brute_domain()) for _ in range(2000)]
@@ -220,6 +224,7 @@ def main():
     parser.add_argument("-s", "--deep", type=int, help='Domain depth', default=5)
     parser.add_argument("-c", "--check_bk", type=bool, help='check  random subdomain', default=True)
     parser.add_argument("-an", "--analysis_domain", type=str, help='analysis cname')
+    parser.add_argument("-fd", "--fuzz_data", type=str, help='FUZZ data')
     parser.add_argument(
         "-n",
         "--next",
@@ -229,6 +234,9 @@ def main():
 
     args = parser.parse_args()
     params = {}
+    if args.fuzz_data:
+        params['fuzz'] = True
+        params['fd'] = args.fuzz_data
     if args.domain is None:
         log.error("Please input domain  such as python subdns.py -u baidu.com")
         sys.exit()
